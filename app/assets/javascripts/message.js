0ff2513e52857f0f5cd4681b52b197e090c31990
message2tomorrow <cosmic.birth@gmail.com>
2019-09-15 20:50:49 +0900

    done3(public/uploads以下はignore)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ function buildHTML(message){
       if ( message.image ) {
         var html =
          `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
+              <div class="upper-message">
               <div class="upper-message__user-name">
                 ${message.user_name}
               </div>
@@ -36,10 +36,30 @@ function buildHTML(message){
                 ${message.content}
               </p>
             </div>
+            <img src=${message.image} >
           </div>`
         return html;
+      } else {
+      var html =
+      `<div class="message" data-message-id=${message.id}>
+           <div class="upper-message">
+             <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+       </div>
+       <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+       </div>`
+       return html;
       };
     }
+
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
