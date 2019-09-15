a11cfff75a1de8e2119d41df61c987c6105dbd48
message2tomorrow <cosmic.birth@gmail.com>
2019-09-15 18:25:28 +0900

    edit of timezone(日本時間へ変更）

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.id @message.id
 json.user_name @message.user.name
-json.date @message.created_at.strftime("%Y/%m/%d/ %H:%M:%S")
+json.date @message.created_at.strftime("%Y/%m/%d(%a) %H:%M:%S")
 json.content @message.content
 json.image @message.image_url
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
-Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
+Date::DATE_FORMATS[:default] = "%Y/%m/%d/"
 Date::DATE_FORMATS[:date] = "%m/%d"
-DateTime::DDATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
-DateTime::DDATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
+DateTime::DATE_FORMATS[:default] = "%Y/%m/%d/ %H/%M:%S"
+DateTime::DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M:%S"
