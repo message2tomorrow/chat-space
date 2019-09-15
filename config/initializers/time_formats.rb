3a6d6161df9049f8b4369e5cbeae77662b65da28
message2tomorrow <cosmic.birth@gmail.com>
2019-09-15 15:05:46 +0900

    View_Change_of_Time_Format(time_format使用）
    修正依頼によりタイムフォーマットを("%Y/%m/%d/ %H:%M:%S")へ変更

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,4 @@
+Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
+Date::DATE_FORMATS[:date] = "%m/%d"
+DateTime::DDATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
+DateTime::DDATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
