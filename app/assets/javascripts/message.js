c098532b1ab058170ab8516d2682321871baf8a2
message2tomorrow <cosmic.birth@gmail.com>
2019-09-19 00:17:24 +0900

    edit_of_jquery (インクリメンタルサーチ)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,6 @@ $(function () {
   }
   $('#new_message').on('submit', function(e){
    e.preventDefault();
-  //  console.log(this);
    var formData = new FormData(this);
    var url = $(this).attr('action')
    $.ajax({
@@ -34,7 +33,6 @@ $(function () {
      contentType: false
    })
     .done(function(data){
-      // console.log(data)
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
